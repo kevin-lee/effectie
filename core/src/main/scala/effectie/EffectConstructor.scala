71bfa8c68b83d7ceee082dee0229854e424b0478
Kevin Lee <kevin.code@kevinlee.io>
2020-03-31 21:47:54 +1100

    Added: Common effect traits

---
+++ core/src/main/scala/effectie/EffectConstructor.scala
@@ -0,0 +1,11 @@
+package effectie
+
+trait EffectConstructor[F[_]] {
+  def effect[A](a: => A): F[A]
+  def pureEffect[A](a: A): F[A]
+  def unit: F[Unit]
+}
+
+object EffectConstructor {
+  def apply[F[_] : EffectConstructor]: EffectConstructor[F] = implicitly[EffectConstructor[F]]
+}
---
+++ core/src/main/scala/effectie/YesNo.scala
@@ -0,0 +1,13 @@
+package effectie
+
+sealed trait YesNo
+
+object YesNo {
+
+  case object Yes extends YesNo
+  case object No extends YesNo
+
+  def yes: YesNo = Yes
+  def no: YesNo = No
+
+}
