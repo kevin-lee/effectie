ff41c1b77254935e433533633fef6ea114aafa86
Kevin Lee <kevin.code@kevinlee.io>
2020-04-04 15:49:45 +1100

    Task #16 - Removed: unnecessary typeclass requirement in EitherTSupport and OptionTSupport

--- cats-effect/src/main/scala/effectie/cats/EitherTSupport.scala
+++ cats-effect/src/main/scala/effectie/cats/EitherTSupport.scala
@@ -2,6 +2,7 @@ package effectie.cats
 
 import cats.Functor
 import cats.data.EitherT
+import cats.implicits._
 
 trait EitherTSupport {
 
@@ -14,8 +15,8 @@ trait EitherTSupport {
   def eitherTLiftEffect[F[_] : EffectConstructor : Functor, A, B](b: => B): EitherT[F, A, B] =
     EitherT.liftF[F, A, B](EffectConstructor[F].effectOf(b))
 
-  def eitherTLiftPureEffect[F[_] : EffectConstructor : Functor, A, B](b: B): EitherT[F, A, B] =
-    EitherT.liftF[F, A, B](EffectConstructor[F].pureEffect(b))
+  def eitherTLiftPureEffect[F[_] : EffectConstructor, A, B](b: B): EitherT[F, A, B] =
+    EitherT(EffectConstructor[F].pureEffect(b.asRight[A]))
 
   def eitherTLiftF[F[_] : EffectConstructor : Functor, A, B](fb: F[B]): EitherT[F, A, B] =
     EitherT.liftF[F, A, B](fb)
