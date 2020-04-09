60f283a8bca168ee520925df185382756d44f9d0
Kevin Lee <kevin.code@kevinlee.io>
2020-04-10 00:39:05 +1000

    Close #41 - Change the names of effect constructor methods in EitherTSupport to effectOf

--- cats-effect/src/main/scala/effectie/cats/EitherTSupport.scala
+++ cats-effect/src/main/scala/effectie/cats/EitherTSupport.scala
@@ -6,13 +6,13 @@ import cats.implicits._
 
 trait EitherTSupport {
 
-  def eitherTEffect[F[_] : EffectConstructor, A, B](ab: => Either[A, B]): EitherT[F, A, B] =
+  def eitherTEffectOf[F[_] : EffectConstructor, A, B](ab: => Either[A, B]): EitherT[F, A, B] =
     EitherT(EffectConstructor[F].effectOf(ab))
 
   def eitherTEffectOfPure[F[_] : EffectConstructor, A, B](ab: Either[A, B]): EitherT[F, A, B] =
     EitherT(EffectConstructor[F].effectOfPure(ab))
 
-  def eitherTLiftEffect[F[_] : EffectConstructor : Functor, A, B](b: => B): EitherT[F, A, B] =
+  def eitherTLiftEffectOf[F[_] : EffectConstructor : Functor, A, B](b: => B): EitherT[F, A, B] =
     EitherT.liftF[F, A, B](EffectConstructor[F].effectOf(b))
 
   def eitherTLiftEffectOfPure[F[_] : EffectConstructor, A, B](b: B): EitherT[F, A, B] =
