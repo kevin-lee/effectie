07ce4f19cb930105d0f50090ea94645a130cc766
Kevin Lee <kevin.code@kevinlee.io>
2020-09-25 22:46:27 +1000

    Close #130 - Add ConsoleEffectful to the implementation projects

---
+++ scalaz-effect/src/main/scala/effectie/scalaz/ConsoleEffectful.scala
@@ -0,0 +1,17 @@
+package effectie.scalaz
+
+import effectie.YesNo
+
+trait ConsoleEffectful {
+
+  def readLn[F[_]: ConsoleEffect]: F[String] = ConsoleEffect[F].readLn
+
+  def putStrLn[F[_]: ConsoleEffect](value: String): F[Unit] = ConsoleEffect[F].putStrLn(value)
+
+  def putErrStrLn[F[_]: ConsoleEffect](value: String): F[Unit] = ConsoleEffect[F].putErrStrLn(value)
+
+  def readYesNo[F[_]: ConsoleEffect](prompt: String): F[YesNo] = ConsoleEffect[F].readYesNo(prompt)
+
+}
+
+object ConsoleEffectful extends ConsoleEffectful
