af3d173b40d3746e15b0935f37cb115dfcc3b8ce
Kevin Lee <kevin.code@kevinlee.io>
2025-09-14 07:44:25 +1000

    Issue #631 - Scala.js support is broken (In Progress) - effectie-cats-effect2 for Scala.js is done - 2

--- modules/effectie-cats-effect2/js/src/test/scala/effectie/instances/ce2/f/fromFutureSpec.scala
+++ modules/effectie-cats-effect2/js/src/test/scala/effectie/instances/ce2/f/fromFutureSpec.scala
@@ -1,43 +1,40 @@
-//package effectie.instances.ce2.f
-//
-//import cats.effect._
-//import effectie.core.FromFuture
-//import effectie.instances.ce2.compat.CatsEffectIoCompatForFuture
-//import extras.concurrent.testing.ConcurrentSupport
-//import extras.concurrent.testing.types.{ErrorLogger, WaitFor}
-//import effectie.instances.ce2.f.fromFuture.fromFutureToAsync
-//import hedgehog._
-//import hedgehog.runner._
-//
-//import scala.concurrent.duration._
-//import scala.concurrent.Future
-//
-///** @author Kevin Lee
-//  * @since 2020-09-22
-//  */
-//object fromFutureSpec extends Properties {
-//  implicit val errorLogger: ErrorLogger[Throwable] = ErrorLogger.printlnDefaultErrorLogger
-//
-//  private val waitFor300Millis = WaitFor(300.milliseconds)
-//
-//  override def tests: List[Test] = List(
-//    property("test FromFuture[IO].toEffect", IoSpec.testToEffect)
-//  )
-//
-//  object IoSpec {
-//    def testToEffect: Property = for {
-//      a <- Gen.int(Range.linear(Int.MinValue, Int.MaxValue)).log("a")
-//    } yield {
-//      val compat = new CatsEffectIoCompatForFuture
-//      import compat.{es, ec, cs}
-//
-//      ConcurrentSupport.runAndShutdown(es, waitFor300Millis) {
-//        lazy val fa = Future(a)
-//        val actual  = FromFuture[IO].toEffect(fa).unsafeRunSync()
-//
-//        actual ==== a
-//      }
-//    }
-//  }
-//
-//}
+package effectie.instances.ce2.f
+
+import cats.effect._
+import effectie.core.FromFuture
+import effectie.instances.ce2.compat.CatsEffectIoCompatForFuture
+import effectie.instances.ce2.f.fromFuture.fromFutureToAsync
+import effectie.testing.{FutureTools, RandomGens}
+import munit.Assertions
+
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+/** @author Kevin Lee
+  * @since 2020-09-22
+  */
+class fromFutureSpec extends munit.FunSuite with FutureTools {
+
+  implicit val ec: ExecutionContext = globalExecutionContext
+
+  override val munitTimeout: FiniteDuration = 200.milliseconds
+
+  test("test FromFuture[IO].toEffect") {
+    val a = RandomGens.genRandomIntWithMinMax(Int.MinValue, Int.MaxValue)
+
+    val compat = new CatsEffectIoCompatForFuture
+    import compat.cs
+
+    val expected = a
+
+    lazy val fa = Future(a)
+    FromFuture[IO]
+      .toEffect(fa)
+      .map { actual =>
+        Assertions.assertEquals(actual, expected)
+      }
+      .unsafeToFuture()
+
+  }
+
+}
