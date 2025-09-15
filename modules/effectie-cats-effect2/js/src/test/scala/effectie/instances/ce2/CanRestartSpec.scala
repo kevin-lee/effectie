df2b12d3a7d397c45e57a6f47bfa00fdb01d4949
Kevin Lee <kevin.code@kevinlee.io>
2025-09-15 12:17:27 +1000

    Issue #631 - Scala.js support is broken (In Progress) - effectie-cats-effect2 for Scala.js is done - 5

--- modules/effectie-cats-effect2/js/src/test/scala/effectie/instances/ce2/CanRestartSpec.scala
+++ modules/effectie-cats-effect2/js/src/test/scala/effectie/instances/ce2/CanRestartSpec.scala
@@ -1,240 +1,231 @@
-//package effectie.instances.ce2
-//
-//import cats.syntax.all._
-//import cats.effect.IO
-//
-//import effectie.core._
-//
-//import hedgehog._
-//import hedgehog.runner._
-//
-//import scala.util.control.NoStackTrace
-//
-///** @author Kevin Lee
-//  * @since 2023-09-07
-//  */
-//class CanRestartSpec extends Properties {
-//  override def tests: List[Test] = List(
-//    property("CanRestart[F].restartWhile", testRestartWhile),
-//    example("CanRestart[F].restartWhile (heavy recursion: 1,000,000 times)", testRestartWhileHeavyRecursion),
-//    property("CanRestart[F].restartUntil", testRestartUntil),
-//    example("CanRestart[F].restartUntil (heavy recursion: 1,000,000 times)", testRestartUntilHeavyRecursion),
-//    property("CanRestart[F].restartOnError", testRestartOnError),
-//    example("CanRestart[F].restartOnError (heavy recursion: 1,000,000 times)", testRestartOnErrorHeavyRecursion),
-//    property("CanRestart[F].restartOnErrorIf", testRestartOnErrorIf),
-//    example("CanRestart[F].restartOnErrorIf (heavy recursion: 1,000,000 times)", testRestartOnErrorIfHeavyRecursion),
-//  )
-//
-//  type F[A] = IO[A]
-//  val F = IO
-//
-//  def canRestartRestartWhile[G[*]: Fx](ga: G[Int], num: Array[Int]): G[Int] =
-//    CanRestart[G].restartWhile(ga) { n =>
-//      if (n != num(0)) {
-//        num(0) = num(0) + 1
-//        true
-//      } else {
-//        false
-//      }
-//    }
-//
-//  def canRestartRestartUntil[G[*]: Fx](ga: G[Int], num: Array[Int]): G[Int] =
-//    CanRestart[G].restartUntil(ga) { n =>
-//      if (n === num(0)) {
-//        true
-//      } else {
-//        num(0) = num(0) + 1
-//        false
-//      }
-//    }
-//
-//  def canRestartRestartOnError[G[*]: Fx](ga: G[Unit], maxRetries: Long): G[Unit] =
-//    CanRestart[G].restartOnError(ga)(maxRetries)
-//
-//  def canRestartRestartOnErrorIfTrue[G[*]: Fx](ga: G[Unit], p: Throwable => Boolean): G[Unit] =
-//    CanRestart[G].restartOnErrorIfTrue(ga)(p)
-//
-//  import effectie.instances.ce2.fx.ioFx
-//
-//  def testRestartWhile: Property =
-//    for {
-//      n <- Gen.int(Range.linear(0, 10)).log("n")
-//    } yield {
-//      val num = Array.fill(1)(0)
-//      canRestartRestartWhile(F(n), num)
-//        .map(_ ==== num(0))
-//        .unsafeRunSync()
-//    }
-//
-//  def testRestartWhileHeavyRecursion: Result = {
-//    val n   = 1000000
-//    val num = Array.fill(1)(0)
-//    canRestartRestartWhile(F(n), num)
-//      .map(_ ==== num(0))
-//      .unsafeRunSync()
-//  }
-//
-//  def testRestartUntil: Property =
-//    for {
-//      n <- Gen.int(Range.linear(0, 10)).log("n")
-//    } yield {
-//      val num = Array.fill(1)(0)
-//      canRestartRestartUntil(F(n), num)
-//        .map(_ ==== num(0))
-//        .unsafeRunSync()
-//    }
-//
-//  def testRestartUntilHeavyRecursion: Result = {
-//    val n   = 1000000
-//    val num = Array.fill(1)(0)
-//    canRestartRestartUntil(F(n), num)
-//      .map(_ ==== num(0))
-//      .unsafeRunSync()
-//  }
-//
-//  def testRestartOnError: Property =
-//    for {
-//      n <- Gen.long(Range.linear(0L, 10L)).log("n")
-//    } yield {
-//      val expected = (n + 1) * 3
-//
-//      val num = Array.fill(1)(0L)
-//
-//      canRestartRestartOnError(
-//        F[Unit] {
-//          num(0) += 1L
-//          ()
-//        }.map { _ =>
-//          num(0) += 1L
-//          ()
-//        }.flatMap { _ =>
-//          F {
-//            num(0) += 1L
-//            ()
-//          }
-//        }.flatMap { _ =>
-//          F.raiseError[Unit](ExpectedException)
-//        },
-//        n,
-//      )
-//        .handleErrorWith {
-//          case ExpectedException => F.unit
-//          case ex => F.raiseError(ex)
-//        }
-//        .unsafeRunSync()
-//      num(0) ==== expected
-//    }
-//
-//  def testRestartOnErrorHeavyRecursion: Result = {
-//    val n = 1000000L
-//
-//    val expected = (n + 1) * 3
-//
-//    val num = Array.fill(1)(0L)
-//
-//    canRestartRestartOnError(
-//      F[Unit] {
-//        num(0) += 1L
-//        ()
-//      }.map { _ =>
-//        num(0) += 1L
-//        ()
-//      }.flatMap { _ =>
-//        F {
-//          num(0) += 1L
-//          ()
-//        }
-//      }.flatMap { _ =>
-//        F.raiseError[Unit](ExpectedException)
-//      },
-//      n,
-//    )
-//      .handleErrorWith {
-//        case ExpectedException => F.unit
-//        case ex => F.raiseError(ex)
-//      }
-//      .unsafeRunSync()
-//    num(0) ==== expected
-//  }
-//
-//  def testRestartOnErrorIf: Property =
-//    for {
-//      n <- Gen.int(Range.linear(0, 10)).log("n")
-//    } yield {
-//      val expected = (n + 1) * 3
-//      val num      = Array.fill(1)(0)
-//
-//      canRestartRestartOnErrorIfTrue(
-//        F[Unit] {
-//          num(0) += 1
-//          ()
-//        }.map { _ =>
-//          num(0) += 1
-//          ()
-//        }.flatMap { _ =>
-//          F {
-//            num(0) += 1
-//            ()
-//          }
-//        }.map[Unit] { _ =>
-//          if (num(0) < expected)
-//            throw KeepGoing // scalafix:ok DisableSyntax.throw
-//          else
-//            throw End // scalafix:ok DisableSyntax.throw
-//        },
-//        {
-//          case KeepGoing => true
-//          case End => false
-//          case err => throw err // scalafix:ok DisableSyntax.throw
-//        },
-//      )
-//        .handleErrorWith {
-//          case End => F.unit
-//          case ex => F.raiseError(ex)
-//        }
-//        .unsafeRunSync()
-//      num(0) ==== expected
-//    }
-//
-//  def testRestartOnErrorIfHeavyRecursion: Result = {
-//    val expected = 1000000 * 3
-//    val num      = Array.fill(1)(0)
-//
-//    canRestartRestartOnErrorIfTrue(
-//      F[Unit] {
-//        num(0) += 1
-//        ()
-//      }.map { _ =>
-//        num(0) += 1
-//        ()
-//      }.flatMap { _ =>
-//        F {
-//          num(0) += 1
-//          ()
-//        }
-//      }.map[Unit] { _ =>
-//        if (num(0) < expected)
-//          throw KeepGoing // scalafix:ok DisableSyntax.throw
-//        else
-//          throw End // scalafix:ok DisableSyntax.throw
-//      },
-//      {
-//        case KeepGoing => true
-//        case End => false
-//        case err => throw err // scalafix:ok DisableSyntax.throw
-//      },
-//    )
-//      .handleErrorWith {
-//        case End => F.unit
-//        case ex => F.raiseError(ex)
-//      }
-//      .unsafeRunSync()
-//    num(0) ==== expected
-//  }
-//
-//  case object KeepGoing extends NoStackTrace
-//  case object End extends NoStackTrace
-//
-//  case object ExpectedException extends NoStackTrace
-//
-//}
+package effectie.instances.ce2
+
+import cats.effect.IO
+import cats.syntax.all._
+import effectie.core._
+import effectie.testing.{FutureTools, RandomGens}
+import hedgehog._
+import munit.Assertions
+
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.util.control.NoStackTrace
+
+/** @author Kevin Lee
+  * @since 2023-09-07
+  */
+class CanRestartSpec extends munit.FunSuite with FutureTools {
+
+  implicit val ec: ExecutionContext = globalExecutionContext
+
+  override val munitTimeout: FiniteDuration = 200.milliseconds
+
+  type F[A] = IO[A]
+  val F = IO
+
+  def canRestartRestartWhile[G[*]: Fx](ga: G[Int], num: Array[Int]): G[Int] =
+    CanRestart[G].restartWhile(ga) { n =>
+      if (n != num(0)) {
+        num(0) = num(0) + 1
+        true
+      } else {
+        false
+      }
+    }
+
+  def canRestartRestartUntil[G[*]: Fx](ga: G[Int], num: Array[Int]): G[Int] =
+    CanRestart[G].restartUntil(ga) { n =>
+      if (n === num(0)) {
+        true
+      } else {
+        num(0) = num(0) + 1
+        false
+      }
+    }
+
+  def canRestartRestartOnError[G[*]: Fx](ga: G[Unit], maxRetries: Long): G[Unit] =
+    CanRestart[G].restartOnError(ga)(maxRetries)
+
+  def canRestartRestartOnErrorIfTrue[G[*]: Fx](ga: G[Unit], p: Throwable => Boolean): G[Unit] =
+    CanRestart[G].restartOnErrorIfTrue(ga)(p)
+
+  import effectie.instances.ce2.fx.ioFx
+
+  test("CanRestart[F].restartWhile") {
+    val n = RandomGens.genRandomIntWithMinMax(0, 10)
+
+    val num = Array.fill(1)(0)
+    canRestartRestartWhile(F(n), num)
+      .map(Assertions.assertEquals(_, num(0)))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartWhile (heavy recursion: 1,000,000 times)") {
+    val n   = 1000000
+    val num = Array.fill(1)(0)
+    canRestartRestartWhile(F(n), num)
+      .map(Assertions.assertEquals(_, num(0)))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartUntil") {
+    val n = RandomGens.genRandomIntWithMinMax(0, 10)
+
+    val num = Array.fill(1)(0)
+    canRestartRestartUntil(F(n), num)
+      .map(Assertions.assertEquals(_, num(0)))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartUntil (heavy recursion: 1,000,000 times)") {
+    val n   = 1000000
+    val num = Array.fill(1)(0)
+    canRestartRestartUntil(F(n), num)
+      .map(_ ==== num(0))
+      .unsafeRunSync()
+  }
+
+  test("CanRestart[F].restartOnError") {
+    val n = RandomGens.genRandomLongWithMinMax(0L, 10L)
+
+    val expected = (n + 1) * 3
+
+    val num = Array.fill(1)(0L)
+
+    canRestartRestartOnError(
+      F[Unit] {
+        num(0) += 1L
+        ()
+      }.map { _ =>
+        num(0) += 1L
+        ()
+      }.flatMap { _ =>
+        F {
+          num(0) += 1L
+          ()
+        }
+      }.flatMap { _ =>
+        F.raiseError[Unit](ExpectedException)
+      },
+      n,
+    )
+      .handleErrorWith {
+        case ExpectedException => F.unit
+        case ex => F.raiseError(ex)
+      }
+      .map(_ => Assertions.assertEquals(num(0), expected))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartOnError (heavy recursion: 1,000,000 times)") {
+    val n = 1000000L
+
+    val expected = (n + 1) * 3
+
+    val num = Array.fill(1)(0L)
+
+    canRestartRestartOnError(
+      F[Unit] {
+        num(0) += 1L
+        ()
+      }.map { _ =>
+        num(0) += 1L
+        ()
+      }.flatMap { _ =>
+        F {
+          num(0) += 1L
+          ()
+        }
+      }.flatMap { _ =>
+        F.raiseError[Unit](ExpectedException)
+      },
+      n,
+    )
+      .handleErrorWith {
+        case ExpectedException => F.unit
+        case ex => F.raiseError(ex)
+      }
+      .map(_ => Assertions.assertEquals(num(0), expected))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartOnErrorIf") {
+    val n = RandomGens.genRandomIntWithMinMax(0, 10)
+
+    val expected = (n + 1) * 3
+    val num      = Array.fill(1)(0)
+
+    canRestartRestartOnErrorIfTrue(
+      F[Unit] {
+        num(0) += 1
+        ()
+      }.map { _ =>
+        num(0) += 1
+        ()
+      }.flatMap { _ =>
+        F {
+          num(0) += 1
+          ()
+        }
+      }.map[Unit] { _ =>
+        if (num(0) < expected)
+          throw KeepGoing // scalafix:ok DisableSyntax.throw
+        else
+          throw End // scalafix:ok DisableSyntax.throw
+      },
+      {
+        case KeepGoing => true
+        case End => false
+        case err => throw err // scalafix:ok DisableSyntax.throw
+      },
+    )
+      .handleErrorWith {
+        case End => F.unit
+        case ex => F.raiseError(ex)
+      }
+      .map(_ => Assertions.assertEquals(num(0), expected))
+      .unsafeToFuture()
+  }
+
+  test("CanRestart[F].restartOnErrorIf (heavy recursion: 1,000,000 times)") {
+    val expected = 1000000 * 3
+    val num      = Array.fill(1)(0)
+
+    canRestartRestartOnErrorIfTrue(
+      F[Unit] {
+        num(0) += 1
+        ()
+      }.map { _ =>
+        num(0) += 1
+        ()
+      }.flatMap { _ =>
+        F {
+          num(0) += 1
+          ()
+        }
+      }.map[Unit] { _ =>
+        if (num(0) < expected)
+          throw KeepGoing // scalafix:ok DisableSyntax.throw
+        else
+          throw End // scalafix:ok DisableSyntax.throw
+      },
+      {
+        case KeepGoing => true
+        case End => false
+        case err => throw err // scalafix:ok DisableSyntax.throw
+      },
+    )
+      .handleErrorWith {
+        case End => F.unit
+        case ex => F.raiseError(ex)
+      }
+      .map(_ => Assertions.assertEquals(num(0), expected))
+      .unsafeToFuture()
+  }
+
+  case object KeepGoing extends NoStackTrace
+  case object End extends NoStackTrace
+
+  case object ExpectedException extends NoStackTrace
+
+}
